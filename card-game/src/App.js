6f16c92bfa7193813fed04b443c72ae6a6561445
Akarapon Oaksonsombat <47691236+akarapon-oaksonsombat@users.noreply.github.com>
2020-08-15 12:05:07 +0700

    Update App.js

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import CharacterCard from './CharacterCard';
 import './App.css';
+const word = "Hello";
 function App() {
  return (
  <div>
+   Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
  <CharacterCard value="h"/>
  <CharacterCard value="i"/>
  </div>
